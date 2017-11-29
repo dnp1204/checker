6dcfe90a27758219070fd88b7a2445e928a93e0c
dnp <doanhpham1995@gmail.com>
2017-11-29 12:09:50 -0500

    make it work

--- src/LocalPlayer.java
+++ src/LocalPlayer.java
@@ -1,17 +1,15 @@
 /**
  * LocalPlayer.java
- *
+ * <p>
  * Version:
- *   $Id: LocalPlayer.java,v 1.1 2002/10/22 21:12:52 se362 Exp $
- *
+ * $Id: LocalPlayer.java,v 1.1 2002/10/22 21:12:52 se362 Exp $
+ * <p>
  * Revisions:
- *   $Log: LocalPlayer.java,v $
- *   Revision 1.1  2002/10/22 21:12:52  se362
- *   Initial creation of case study
- *
+ * $Log: LocalPlayer.java,v $
+ * Revision 1.1  2002/10/22 21:12:52  se362
+ * Initial creation of case study
  */
 
-import java.awt.*;
 import javax.swing.*;
 
 /**
@@ -23,62 +21,62 @@ import javax.swing.*;
  */
 
 public class LocalPlayer extends Player {
-    
+
     /**
      * This is a default constructor for this object
      */
-    public LocalPlayer( int num, Rules rules, Driver theDriver ){
-	super( num, rules, theDriver );
-	type = Player.LOCALPLAYER;
+    public LocalPlayer(int num, Rules rules, Driver theDriver) {
+        super(num, rules, theDriver);
+        type = Player.LOCALPLAYER;
     }
-    
+
     /**
      * When the current player clicks the draw button, this method 
      * is called in the opposite player to inform them that a draw 
      * has been offered. An actionEvent is generated to let the GUI 
      * know.
      */
-    public void offerDraw( Player player ){
-	theDriver.drawOffered( this );
+    public void offerDraw(Player player) {
+        theDriver.drawOffered(this);
     }
-    
+
     /**
      * When the current player accepts a draw, this method is called 
      * in the opposite player to inform them that the draw has been 
      * accepted.  An actionEvent is generated to let the GUI know and 
      * endGame is called in theDriver.
      */
-    public void acceptDraw( Player player ){
-	
-	int selected = JOptionPane.showConfirmDialog( null, player.getName()
-		   	      + " has requested a draw."
-	       		      + "\n\nWill you agree to a"
-	      		      + " draw?",
-	       		      "Draw offer",
-	     	       	      JOptionPane.YES_NO_OPTION );
-	
-	if ( selected == JOptionPane.YES_OPTION ) {
-	    theDriver.endInDraw( player );
-	} else if ( selected == JOptionPane.NO_OPTION ) {
-	    theDriver.declineDraw( player );
-	} else {
-	    theDriver.declineDraw( player );
-	}
+    public void acceptDraw(Player player) {
+
+        int selected = JOptionPane.showConfirmDialog(null, player.getName()
+                        + " has requested a draw."
+                        + "\n\nWill you agree to a"
+                        + " draw?",
+                "Draw offer",
+                JOptionPane.YES_NO_OPTION);
+
+        if (selected == JOptionPane.YES_OPTION) {
+            theDriver.endInDraw(player);
+        } else if (selected == JOptionPane.NO_OPTION) {
+            theDriver.declineDraw(player);
+        } else {
+            theDriver.declineDraw(player);
+        }
     }
-    
+
     /**
      *  Method is invoked if the other player declines a draw.
      */
-    public void endInDeclineDraw( Player player ){
-	
-	JOptionPane.showMessageDialog( null,
-       	            player.getName() + " has declined the draw offer."
-                    + "\n\nClick OK to continue the game.",
-	            "Draw declined",
-	            JOptionPane.INFORMATION_MESSAGE );
-	
+    public void endInDeclineDraw(Player player) {
+
+        JOptionPane.showMessageDialog(null,
+                player.getName() + " has declined the draw offer."
+                        + "\n\nClick OK to continue the game.",
+                "Draw declined",
+                JOptionPane.INFORMATION_MESSAGE);
+
     }
-    
+
     /**
      * Method that is invoked when the end of game conditions have 
      * been met.  Fire off an action event to tell the GUI to display 
@@ -87,18 +85,18 @@ public class LocalPlayer extends Player {
      *
      * @param endMessage  Message indicating the end of the game.
      */
-    public void endOfGame( String endMessage ){
-	
-	JOptionPane.showMessageDialog( null,
-               "Game has ended because: "
-       	       + endMessage,
-       	       "Game Over",
-	       JOptionPane.INFORMATION_MESSAGE );
-	
-	System.exit( 0 );
-	
+    public void endOfGame(String endMessage) {
+
+        JOptionPane.showMessageDialog(null,
+                "Game has ended because: "
+                        + endMessage,
+                "Game Over",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
+
     }
-    
+
     /**
      * Method that is invoked when the end of game conditions have 
      * been met.  If they have been, this method is called in both 
@@ -107,18 +105,18 @@ public class LocalPlayer extends Player {
      *
      * @param endMessage  Message indicating the end of the game.
      */
-    public void endInDraw( Player player ){
-	
-	// This display end of game dialog telling that a game has 
-	// ended in a draw
-	JOptionPane.showMessageDialog( null,
-       	       player.getName() + " accepted a draw."
-	       + "\n\nClick OK to end the program.",
-      	       "Game Over",
-               JOptionPane.INFORMATION_MESSAGE );
-	
-	System.exit( 0 );
+    public void endInDraw(Player player) {
+
+        // This display end of game dialog telling that a game has
+        // ended in a draw
+        JOptionPane.showMessageDialog(null,
+                player.getName() + " accepted a draw."
+                        + "\n\nClick OK to end the program.",
+                "Game Over",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
     }
-    
+
 }//LocalPlayer.java
 
--- src/Timer.java
+++ src/Timer.java
@@ -1,14 +1,13 @@
 /**
  * Timer.java
- *
+ * <p>
  * Version:
- *    $Id: Timer.java,v 1.1 2002/10/22 21:12:53 se362 Exp $
- *
+ * $Id: Timer.java,v 1.1 2002/10/22 21:12:53 se362 Exp $
+ * <p>
  * Revisions:
- *    $Log: Timer.java,v $
- *    Revision 1.1  2002/10/22 21:12:53  se362
- *    Initial creation of case study
- *
+ * $Log: Timer.java,v $
+ * Revision 1.1  2002/10/22 21:12:53  se362
+ * Initial creation of case study
  */
 
 /**
@@ -20,59 +19,58 @@
  *  @author
  */
 
-public class Timer extends Thread{
+public class Timer extends Thread {
 
-    private static int INTERVAL = 100;    
-    private int      interval;
+    private static int INTERVAL = 100;
+    private int interval;
     private Notifier notifier;
-    
+
     /**
      * This constaructor creates a new timer
-     */    
-    public Timer(){
-	notifier = new Notifier( notifier.TIME_UPDATE );
-	interval = INTERVAL;
+     */
+    public Timer() {
+        notifier = new Notifier(notifier.TIME_UPDATE);
+        interval = INTERVAL;
     }
-    
+
     /**
      * This constructor creates a timer with an interval
      *
      * @param inter - the new interval
      */
-    public Timer( int inter ){
-	notifier = new Notifier( notifier.TIME_UPDATE );
-	interval = inter;
+    public Timer(int inter) {
+        notifier = new Notifier(notifier.TIME_UPDATE);
+        interval = inter;
     }
-    
+
     /**
      * This method is what executes then the thread
      * has been started
-     * 
+     *
      * @roseuid 3C5AE02D03DE
      */
     public void run() {
-	// Start the timer thread 
-	// Notify the facade every interval
-	while ( true ) {
-	    try {
-		sleep( interval );
-	    }
-	    catch ( InterruptedException e ) {
-		System.err.println( "The timer malfunctioned." );
-	    }
-	    
-	    notifier.generateActionPerformed();
-	}
+        // Start the timer thread
+        // Notify the facade every interval
+        while (true) {
+            try {
+                sleep(interval);
+            } catch (InterruptedException e) {
+                System.err.println("The timer malfunctioned.");
+            }
+
+            notifier.generateActionPerformed();
+        }
     }
-    
+
     /**
      * Get the notifier.
-     * 
+     *
      * @return Notifier
      * @roseuid 3C5AE4FD00C1
      */
-    public Notifier getNotifier(){
-	return notifier;
+    public Notifier getNotifier() {
+        return notifier;
     }
-    
+
 }// Timer.java
